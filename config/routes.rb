f2213a5e471b59de7dc09799299c502f2b6e9886
rdutra <rdutra@timbasoftware.com>
2011-12-29 12:33:09 -0200

    added oAuth2 salesforce autentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Chat::Application.routes.draw do
  
+  get "authorization/create"
+ 
   match '/auth/:provider/callback', :to => 'authorization#create'
   match '/auth/failure', :to => 'authorization#fail'
   
