38be193359ce90ff38dde4714996285a2ab8ea62
rdutra <rdutra@timbasoftware.com>
2012-01-05 16:07:27 -0200

    added salesforce user functions created status algorithms(unfinished)

--- app/models/buddy.rb
+++ app/models/buddy.rb
@@ -3,17 +3,42 @@ include HTTParty
 
   format :json
   
+  STATUSES = %w{ Available Away Busy Offline } 
+  
   def get_buddy_by_id buddy_id
     buddy = Buddy.where(:id => buddy_id)[0]
     return buddy
   end
   
-  def add_buddy buddy
-    new_buddy = Buddy.new
-    new_buddy.name = buddy[:name]
-    new_buddy.nickname = buddy[:nickname]
-    new_buddy.status = buddy[:status]
-    new_buddy.save
+  def self.add_buddy buddy
+    
+  end
+  
+  def set_status id , status
+    buddy = Buddy.where(:id => buddy_id)[0]  
+    if STATUSES.include? status
+      buddy.status = status
+      buddy.save
+    end
+    return buddy
+  end
+  
+  def self.get_buddy_by_Sfid sfid
+    buddy = Buddy.where(:salesforce_id => sfid)
+    unless buddy.empty?
+      return buddy
+    else
+      return false
+    end
+  end
+  
+  def self.exists_buddy sfid
+    buddy = Buddy.where(:salesforce_id => sfid)
+    unless buddy.empty?
+      return true
+    else
+      return false
+    end
   end
   
 end
