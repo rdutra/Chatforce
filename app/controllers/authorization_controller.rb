8bb89fe3b665960d0b67425c573b6655b8a650cf
rdutra <rdutra@timbasoftware.com>
2012-01-09 10:07:15 -0200

    Fixed set available on login

--- app/controllers/authorization_controller.rb
+++ app/controllers/authorization_controller.rb
@@ -5,20 +5,20 @@ class AuthorizationController < ApplicationController
     ENV['sfdc_token'] = request.env['omniauth.auth']['credentials']['token']
     ENV['sfdc_instance_url'] = request.env['omniauth.auth']['instance_url']
     sfuser = Users.getMe
-    if not Buddy.exists_buddy sfuser[:id]
+    if not Buddy.exists_buddy sfuser["id"]
       new_user = Buddy.new({
-        :name             => sfuser[:name],
+        :name             => sfuser["name"],
         :nickname         => '',
         :status           => "Available",
-        :salesforce_id    => sfuser[:id],
-        :small_photo_url  => sfuser[:smallPhotoUrl]
+        :salesforce_id    => sfuser["id"],
+        :small_photo_url  => sfuser["smallPhotoUrl"]
       })
       new_user.save
     else
-      new_user = Buddy.get_buddy_by_Sfid sfuser[:id]
+      new_user = Buddy.get_buddy_by_Sfid sfuser["id"]
     end
     if new_user
-      #new_user.set_status new_user.id "Available"
+      Buddy.set_status sfuser["id"], "Available"
     end
     redirect_to :controller => 'buddies', :action => 'index'
   end
