a1aaf550e4aae1fd6299b2ee572fb904a5ffd95a
mmeyer <mmeyer@timbasoftware.com>
2012-02-21 17:46:17 -0200

    added channel selected on the load of the buddy page

--- app/controllers/buddies_controller.rb
+++ app/controllers/buddies_controller.rb
@@ -13,7 +13,6 @@ class BuddiesController < ApplicationController
     @org_channel = Channel.get_ring_communication @session["buddy_id"], org_id
     Connection.connect_buddy @session["buddy_id"], @org_channel[:id]
     
-    # seguir aca
     mychannels = Channel.joins(:connection).where(:connections => {:buddy_id => 2}, :mod =>  'chat')
     @myBuddiesMap = {}
     myBuddiesConnection = Connection.where 'channel_id IN (:mychannels) AND buddy_id <> :myBuddy', {:mychannels => mychannels, :myBuddy => 2}
